65583cf3a6734a0f42138c57f8ffb5206d58b9cf
selcukusta <selcukusta@gmail.com>
2020-12-05 15:20:36 +0300

    Using validator package for passport validation

--- day_4/puz_2/main.go
+++ day_4/puz_2/main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
+	"reflect"
 	"regexp"
 	"strconv"
 	"strings"
+
+	"gopkg.in/validator.v2"
 )
+
+type passport struct {
+	BirthYear      int    `validate:"min=1920,max=2002"`
+	IssueYear      int    `validate:"min=2010,max=2020"`
+	ExpirationYear int    `validate:"min=2020,max=2030"`
+	Height         string `validate:"nonzero,height"`
+	HairColor      string `validate:"nonzero,regexp=^#(?:[0-9a-f]{3}){2}$"`
+	EyeColor       string `validate:"nonzero,regexp=^(amb|blu|brn|gry|grn|hzl|oth){1}$"`
+	PassportID     string `validate:"nonzero,regexp=^[0-9]{9}$"`
+}
 
 // Read is using to read a whole file into memory and return a slice.
 func Read(path string) ([]string, bool) {
@@ -25,15 +39,6 @@ func Read(path string) ([]string, bool) {
 	return lines, true
 }
 
-func contains(slice []string, val string) bool {
-	for _, item := range slice {
-		if item == val {
-			return true
-		}
-	}
-	return false
-}
-
 func validRange(val string, min int, max int) bool {
 	numericValue, err := strconv.Atoi(val)
 	if err != nil || !(numericValue >= min && numericValue <= max) {
@@ -42,23 +47,60 @@ func validRange(val string, min int, max int) bool {
 	return true
 }
 
-func validRegex(val string, pattern *regexp.Regexp) bool {
-	return pattern.Match([]byte(val))
+func getStringOrDefault(model map[string]interface{}, key string) string {
+	val, ok := model[key]
+	if ok {
+		return val.(string)
+	}
+	return ""
+}
+
+func getIntOrDefault(model map[string]interface{}, key string) int {
+	val, ok := model[key]
+	if ok {
+		conv, err := strconv.Atoi(val.(string))
+		if err != nil {
+			return 0
+		}
+		return conv
+	}
+	return 0
+}
+
+func heightValidator(v interface{}, param string) error {
+	st := reflect.ValueOf(v)
+	if st.Kind() != reflect.String {
+		return validator.ErrUnsupported
+	}
+
+	val := st.String()
+	if !strings.HasSuffix(val, "cm") && !strings.HasSuffix(val, "in") {
+		return errors.New("Undefined unit")
+	}
+
+	if strings.HasSuffix(val, "cm") && !validRange(strings.TrimSuffix(val, "cm"), 150, 193) {
+		return errors.New("Invalid range for cm")
+	}
+
+	if strings.HasSuffix(val, "in") && !validRange(strings.TrimSuffix(val, "in"), 59, 76) {
+		return errors.New("Invalid range for in")
+	}
+
+	return nil
 }
 
 func main() {
+	validator.SetValidationFunc("height", heightValidator)
 	lines, ok := Read("input.txt")
 	if !ok {
 		panic("stop")
 	}
 
 	var (
-		modelPattern        = regexp.MustCompile(`(?P<key>byr|iyr|eyr|hgt|hcl|ecl|pid|cid):(?P<value>\S+)`)
-		hexPattern          = regexp.MustCompile(`^#(?:[0-9a-f]{3}){2}$`)
-		paddedNumberPattern = regexp.MustCompile(`^\d{9}$`)
-		valid               = 0
-		passports           = make([]map[string]interface{}, 0)
-		temp                = make(map[string]interface{})
+		modelPattern = regexp.MustCompile(`(?P<key>byr|iyr|eyr|hgt|hcl|ecl|pid|cid):(?P<value>\S+)`)
+		valid        = 0
+		passports    = make([]map[string]interface{}, 0)
+		temp         = make(map[string]interface{})
 	)
 
 	for idx, line := range lines {
@@ -76,66 +118,21 @@ func main() {
 	}
 
 	for _, v := range passports {
-		if len(v) != 7 {
+		p := &passport{
+			BirthYear:      getIntOrDefault(v, "byr"),
+			IssueYear:      getIntOrDefault(v, "iyr"),
+			ExpirationYear: getIntOrDefault(v, "eyr"),
+			Height:         getStringOrDefault(v, "hgt"),
+			HairColor:      getStringOrDefault(v, "hcl"),
+			EyeColor:       getStringOrDefault(v, "ecl"),
+			PassportID:     getStringOrDefault(v, "pid"),
+		}
+
+		err := validator.Validate(p)
+		if err != nil {
 			continue
-		}
-
-		val, ok := v["byr"]
-		if !ok || !validRange(val.(string), 1920, 2002) {
-			continue
-		}
-
-		val, ok = v["iyr"]
-		if !ok || !validRange(val.(string), 2010, 2020) {
-			continue
-		}
-
-		val, ok = v["eyr"]
-		if !ok || !validRange(val.(string), 2020, 2030) {
-			continue
-		}
-
-		val, ok = v["hgt"]
-		if ok {
-			literalValue, ok := val.(string)
-			if !ok || !(strings.HasSuffix(literalValue, "cm") || strings.HasSuffix(literalValue, "in")) {
-				continue
-			}
-
-			if strings.HasSuffix(literalValue, "cm") && !validRange(strings.TrimSuffix(literalValue, "cm"), 150, 193) {
-				continue
-			}
-
-			if strings.HasSuffix(literalValue, "in") && !validRange(strings.TrimSuffix(literalValue, "in"), 59, 76) {
-				continue
-			}
-		}
-
-		val, ok = v["hcl"]
-		if ok {
-			literalValue, ok := val.(string)
-			if !ok || !validRegex(literalValue, hexPattern) {
-				continue
-			}
-		}
-
-		val, ok = v["ecl"]
-		if ok {
-			literalValue, ok := val.(string)
-			if !ok || !contains(strings.Split("amb blu brn gry grn hzl oth", " "), literalValue) {
-				continue
-			}
-		}
-
-		val, ok = v["pid"]
-		if ok {
-			literalValue, ok := val.(string)
-			if !ok || !validRegex(literalValue, paddedNumberPattern) {
-				continue
-			}
 		}
 		valid++
 	}
-
 	fmt.Println(valid)
 }
